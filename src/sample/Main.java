5e316bdc3704338e1243141e288bfb0adff317db
snasir8 <snasir8@gatech.edu>
2021-02-22 03:45:53 -0500

    comment out unneeded and problematic line (Parent root =...)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         String[] difficulties = {"Easy", "!Easy", "!!Easy"};
         String[] weapons = {"Sword", "Bow", "Broken Bottle"};
         Button configToInitialSceneButton = new Button("Let's go!");
