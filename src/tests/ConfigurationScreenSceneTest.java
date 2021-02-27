089b53e7d5b97be04aaa415fc716d77f59a61480
Cooper, Shane O'hanlon <scooper72@gatech.edu>
2021-02-27 14:04:07 -0500

    add TestFX for selecting each weapon type

--- src/tests/ConfigurationScreenSceneTest.java
+++ src/tests/ConfigurationScreenSceneTest.java
@@ -14,7 +14,7 @@ import static org.testfx.api.FxAssert.verifyThat;
 public class ConfigurationScreenSceneTest extends ApplicationTest {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Main controller = new Main();
         controller.start(primaryStage);
     }
@@ -22,5 +22,31 @@ public class ConfigurationScreenSceneTest extends ApplicationTest {
     public void testPlay() {
         clickOn("Start");
         verifyThat("Bar Crawler", NodeMatchers.isNotNull());
+    }
+
+    @Test
+    public void selectBrokenBottle() {
+        clickOn("Start");
+        write("Scoops");
+        clickOn("Broken Bottle");
+        clickOn("Let's go!");
+        verifyThat("Broken Bottle", NodeMatchers.isNotNull());
+    }
+
+    @Test
+    public void selectBow() {
+        clickOn("Start");
+        write("Scoops");
+        clickOn("Bow");
+        clickOn("Let's go!");
+        verifyThat("Bow", NodeMatchers.isNotNull());
+    }
+
+    @Test
+    public void selectSword() {
+        clickOn("Start");
+        write("Scoops");
+        clickOn("Let's go!");
+        verifyThat("Sword", NodeMatchers.isNotNull());
     }
 }
