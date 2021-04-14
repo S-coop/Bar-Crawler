902d927947e0a55dec72f948b581db27d4e427e6
snasir8 <snasir8@gatech.edu>
2021-04-14 14:31:59 -0400

    add Bella's complete M5 tests

---
+++ src/tests/BellaM5Tests.java
@@ -0,0 +1,46 @@
+package tests;
+
+
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+import src.controller.Main;
+import src.view.AttackPotionView;
+
+import static org.junit.Assert.*;
+
+public class BellaM5Tests extends ApplicationTest {
+    private Main controller;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        controller = new Main();
+        controller.start(primaryStage);
+        primaryStage.show();
+    }
+
+    @Test
+    public void testInitialMultipler() {
+        clickOn("Start");
+        write("username");
+        clickOn("Let's go!");
+        double multiplier = controller.getPlayerView().getModel().getDamageMultiplier();
+        assertTrue(multiplier == 1.0);
+    }
+
+    @Test
+    public void testAttackMultiplier() {
+        clickOn("Start");
+        write("username");
+        clickOn("Let's go!");
+
+        AttackPotionView hp = new AttackPotionView(
+                new Image("file:assets/inventory_items/health.png"));
+        hp.useItem(controller.getPlayerView());
+        double multiplier = controller.getPlayerView().getModel().getDamageMultiplier();
+        System.out.println("The damage multipler is: " + multiplier);
+        assertTrue(multiplier != 1.0);
+    }
+
+}
